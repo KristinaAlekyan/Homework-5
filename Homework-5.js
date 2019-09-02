ebbdf942c1c0d50e4409bb4ab478f35e2c8c1e57
KristinaAlekyan <53223679+KristinaAlekyan@users.noreply.github.com>
2019-09-02 15:44:57 +0400

    Add files via upload

---
+++ Homework-5.js
@@ -0,0 +1,30 @@
+/*3. Given a word and a list of possible anagrams, select the correct sublist.*/
+
+
+function arrSub(arr, arrCheked){
+let a;
+let b;
+
+	if(a.length=b.length ){
+
+		let checked=false;
+
+		for(let i=0; i < a.length; i++){
+
+		if (!a[i].includes(b)){
+
+		break;
+		}
+		
+else { 
+	
+	checked=true;
+		}		
+	}
+
+	
+if (checked=true)
+return b;
+}
+ 
+console.log(arrSub("listen", "istenl"));
